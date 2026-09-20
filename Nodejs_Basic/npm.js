bc7d725859db7fe11206426fcfd10df30c182e40
Abhay-Tewatia <abhaytewtia342@gmail.com>
2026-09-20 17:00:28 +0530

    NPM Basics and Avance Features

---
+++ Nodejs_Basic/npm.js
@@ -0,0 +1,10 @@
+// NPM understanding
+
+// node js core mein jo installed aata hai wo kehlaata hain module
+
+// npm se download krte hai wo hota hai package
+
+// text to speech google ne already krkee rakha hai joki node js per hai already/
+
+// installing and uninstalling
+
