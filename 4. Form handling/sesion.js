84e5e2f6af0d0998f33f98d3af872d7132afb5ed
Abhay-Tewatia <abhaytewtia342@gmail.com>
2026-09-24 11:22:14 +0530

    seesion cookies from scratch

---
+++ 4. Form handling/sesion.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use(function (req, res, next) {
+    console.log("middleware chala");
+    next();
+});
+
+app.use(function (req, res, next) {
+    console.log("middleware chala ek aur baar");
+    next();
+});
+
+// Routes
+app.get("/", function (req, res) {
+    res.send("champion mera abhay");
+});
+
+app.get("/profile", function (req, res) {
+    res.send("champion mera abhay node js seekhi");
+});
+
+// Error testing route
+app.get("/error", function (req, res, next) {
+    const err = new Error("Chacha error aa gaya!");
+    next(err);
+});
+
+// Error-handling middleware
+app.use((err, req, res, next) => {
+    console.log("Error aa gaya:", err.message);
+
+    res.status(500).json({
+        success: false,
+        message: "Kuch toh gadbad hai chacha 😄"
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server started at http://localhost:3000");
+});
